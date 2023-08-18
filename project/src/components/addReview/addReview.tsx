51e7e9c9b5c67c92396e4b73be08e8a13c17b506
Zhanbek Sarsenov <zhanbek.sarsenov@gmail.com>
2023-08-18 23:08:01 +0600

    Добавляет компонент MoviePageDetails

--- project/src/components/addReview/addReview.tsx
+++ project/src/components/addReview/addReview.tsx
@@ -180,9 +180,7 @@ export default function AddReview(): JSX.Element {
           <div className="add-review__text">
             <textarea className="add-review__textarea" name="review-text" id="review-text" placeholder="Review text"></textarea>
             <div className="add-review__submit">
-              <button className="add-review__btn" type="submit">
-                Post
-              </button>
+              <button className="add-review__btn" type="submit">Post</button>
             </div>
           </div>
         </form>
