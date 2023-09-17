5203b5426ff5edd2c365b1e2117a1b13f96977e2
Zhanbek Sarsenov <zhanbek.sarsenov@gmail.com>
2023-09-18 03:50:34 +0600

    Adds Page404 component

---
+++ project/src/components/page-404/page-404.tsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+import MainPage from "../../pages/main-page/main-page";
+
+export default function Page404() {
+  return (
+    <>
+      <h1>Ошибка 404. Страница не существует.</h1>
+      <Link to="/MainPage">Перейти на главную страницу</Link>
+    </>
+  );
+}
