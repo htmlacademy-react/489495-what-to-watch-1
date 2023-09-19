3c20406771670835598ba35c8f7d197100c45958
Zhanbek Sarsenov <zhanbek.sarsenov@gmail.com>
2023-09-20 00:43:33 +0600

    Fixes bugs

--- project/src/components/page-404/page-404.tsx
+++ project/src/components/page-404/page-404.tsx
@@ -1,11 +1,13 @@
-import { Link } from "react-router-dom";
-import MainPage from "../../pages/main-page/main-page";
+import { Link } from 'react-router-dom';
+import MainPage from '../../pages/main-page/main-page';
 
 export default function Page404() {
   return (
     <>
       <h1>Ошибка 404. Страница не существует.</h1>
-      <Link to="/MainPage">Перейти на главную страницу</Link>
+      <Link to={<MainPage title={''} genre={''} issueDate={''} />}>
+        Перейти на главную страницу
+      </Link>
     </>
   );
 }
