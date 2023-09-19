962beb427ae70af850d040be6e9ff39c576a1608
Zhanbek Sarsenov <zhanbek.sarsenov@gmail.com>
2023-09-19 22:34:37 +0600

    Adds PrivateRoute component

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,14 +1,13 @@
-import { AppRoute } from '../../const';
-import MainPage from '../../pages/main-page/main-page';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AppRoute, AuthorizationStatus } from '../../const';
+import MainPage from '../../pages/main-page/main-page';
 import SignIn from '../sign-in/sign-in';
 import MyList from '../my-list/my-list';
-// import MoviePage from '../movie-page/movie-page';
-// import Player from '../player/player';
-import Page404 from '../page-404/page-404';
 import MoviePage from '../movie-page/movie-page';
 import AddReview from '../add-review/add-review';
 import Player from '../player/player';
+import Page404 from '../page-404/page-404';
+import PrivateRoute from '../private-route/private-route';
 
 type AppProps = {
   movieTitle: string;
@@ -32,12 +31,23 @@ function App({ movieTitle, movieGenre, issueYear }: AppProps): JSX.Element {
             }
           />
           <Route path={AppRoute.SignIn} element={<SignIn />} />
-          <Route path={AppRoute.MyList} element={<MyList />} />
+
+          <Route
+            path={AppRoute.MyList}
+            element={
+              <PrivateRoute authorizationStatus={AuthorizationStatus.NoAuth}>
+                <MyList />
+              </PrivateRoute>
+            }
+          />
+
           <Route path={AppRoute.Films}>
+            <Route index element={<Page404 />} />
             <Route path={AppRoute.Film}>
               <Route index element={<MoviePage />} />
               <Route path={AppRoute.AddReview} element={<AddReview />} />
             </Route>
+            <Route path="*" element={<Page404 />} />
           </Route>
           <Route path={AppRoute.Player} element={<Player />} />
         </Route>
--- project/src/const.ts
+++ project/src/const.ts
@@ -2,8 +2,14 @@ export enum AppRoute {
   Main = '/',
   SignIn = '/login',
   MyList = '/mylist',
-  Films = '/films',
-  Film = '/:id',
-  AddReview = '/review',
-  Player = '/player/:id'
+  Films = '/films/',
+  Film = ':id/',
+  AddReview = 'review',
+  Player = 'player/:id'
+}
+
+export enum AuthorizationStatus {
+  Auth = 'AUTH',
+  NoAuth = 'NO_AUTH',
+  Unknown = 'UNKNOWN'
 }
